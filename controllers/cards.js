cdbf7fac8b6b8c69298b48fe8ed53889e984c30e
Maria Golovatenko <pikch4y@yandex.com>
2023-07-13 14:12:38 +0300

    feat: add status code consts

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,36 +1,59 @@
 const Card = require('../models/card');
 
+const {
+  SUCCESS_CODE, SUCCESS_CREATE_CODE, WRONG_DATA_CODE, NOT_FOUND_CODE, SERVER_ERROR_CODE,
+} = require('../utils/constants');
+
 module.exports.getCards = (req, res) => {
   Card.find({})
-    .then((cards) => res.send(cards))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((cards) => res.status(SUCCESS_CODE).res.send(cards))
+    .catch(() => res.status(SERVER_ERROR_CODE).send({ message: 'Серверная ошибка' }));
 };
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   const owner = req.user._id;
+
   Card.create({ name, link, owner })
-    .then((card) => res.status(201).send(card))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((card) => res.status(SUCCESS_CREATE_CODE).send(card))
+    .catch(() => res.status(SERVER_ERROR_CODE).send({ message: 'Серверная ошибка' }));
 };
 
 module.exports.deleteCard = (req, res) => {
   const { cardId } = req.params;
+
   Card.findByIdAndRemove(cardId)
-    .then((card) => res.status(201).send(card))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((card) => {
+      if (!card) {
+        return res.status(NOT_FOUND_CODE).send({ message: 'Карточка с указанным _id не найдена.' });
+      }
+      return res.status(SUCCESS_CODE).send(card);
+    })
+    .catch(() => res.status(SERVER_ERROR_CODE).send({ message: 'Серверная ошибка' }));
 };
 
 module.exports.likeCard = (req, res) => {
   const { cardId } = req.params;
+
   Card.findByIdAndUpdate(cardId, { $addToSet: { likes: req.user._id } }, { new: true })
-    .then((card) => res.status(201).send(card))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((card) => {
+      if (!card) {
+        return res.status(NOT_FOUND_CODE).send({ message: 'Передан несуществующий _id карточки.' });
+      }
+      return res.status(SUCCESS_CODE).send(card);
+    })
+    .catch(() => res.status(SERVER_ERROR_CODE).send({ message: 'Серверная ошибка' }));
 };
 
 module.exports.unlikeCard = (req, res) => {
   const { cardId } = req.params;
+
   Card.findByIdAndUpdate(cardId, { $pull: { likes: req.user._id } }, { new: true })
-    .then((card) => res.status(201).send(card))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((card) => {
+      if (!card) {
+        return res.status(NOT_FOUND_CODE).send({ message: 'Передан несуществующий _id карточки.' });
+      }
+      return res.status(SUCCESS_CODE).send(card);
+    })
+    .catch(() => res.status(SERVER_ERROR_CODE).send({ message: 'Серверная ошибка' }));
 };
--- controllers/users.js
+++ controllers/users.js
@@ -1,37 +1,60 @@
 const User = require('../models/user');
 
+const {
+  SUCCESS_CODE, SUCCESS_CREATE_CODE, WRONG_DATA_CODE, NOT_FOUND_CODE, SERVER_ERROR_CODE,
+} = require('../utils/constants');
+
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.send(users))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((users) => res.status(SUCCESS_CODE).res.send(users))
+    .catch((err) => res.status(SERVER_ERROR_CODE).send({ message: err.message }));
 };
 
 module.exports.getUserById = (req, res) => {
   const userId = req.user._id;
+
   User.findById(userId)
-    .then((user) => res.status(201).send(user))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((user) => {
+      if (!user) {
+        return res.status(NOT_FOUND_CODE).send({ message: ' Пользователь по указанному _id не найден. ' });
+      }
+      return res.status(SUCCESS_CODE).send(user);
+    })
+    .catch((err) => res.status(SERVER_ERROR_CODE).send({ message: err.message }));
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
+
   User.create({ name, about, avatar })
-    .then((user) => res.status(201).send(user))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((user) => res.status(SUCCESS_CREATE_CODE).send(user))
+    .catch((err) => res.status(SERVER_ERROR_CODE).send({ message: err.message }));
 };
 
 module.exports.changeProfile = (req, res) => {
   const { name, about } = req.body;
   const userId = req.user._id;
+
   User.findByIdAndUpdate(userId, { name, about }, { new: true })
-    .then((user) => res.status(201).send(user))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((user) => {
+      if (!user) {
+        return res.status(NOT_FOUND_CODE).send({ message: 'Пользователь с указанным _id не найден. ' });
+      }
+      return res.status(SUCCESS_CODE).send(user);
+    })
+    .catch((err) => res.status(SERVER_ERROR_CODE).send({ message: err.message }));
 };
 
 module.exports.changeAvatar = (req, res) => {
   const { avatar } = req.body;
   const userId = req.user._id;
+
   User.findByIdAndUpdate(userId, { avatar }, { new: true })
-    .then((user) => res.status(201).send(user))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((user) => {
+      if (!user) {
+        return res.status(NOT_FOUND_CODE).send({ message: 'Пользователь с указанным _id не найден. ' });
+      }
+      return res.status(SUCCESS_CODE).send(user);
+    })
+    .catch((err) => res.status(SERVER_ERROR_CODE).send({ message: err.message }));
 };
