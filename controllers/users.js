12121caf8eccefb67d21bc62cb97473d5e6081b3
Maria Golovatenko <pikch4y@yandex.com>
2023-07-13 12:02:08 +0300

    hotfix: fix patch methods

--- controllers/users.js
+++ controllers/users.js
@@ -7,7 +7,7 @@ module.exports.getUsers = (req, res) => {
 };
 
 module.exports.getUserById = (req, res) => {
-  const { userId } = req.params;
+  const userId = req.user._id;
   User.findById(userId)
     .then((user) => res.status(201).send(user))
     .catch((err) => res.status(500).send({ message: err.message }));
@@ -22,7 +22,7 @@ module.exports.createUser = (req, res) => {
 
 module.exports.changeProfile = (req, res) => {
   const { name, about } = req.body;
-  const { userId } = req.user._id;
+  const userId = req.user._id;
   User.findByIdAndUpdate(userId, { name, about }, { new: true })
     .then((user) => res.status(201).send(user))
     .catch((err) => res.status(500).send({ message: err.message }));
@@ -30,7 +30,7 @@ module.exports.changeProfile = (req, res) => {
 
 module.exports.changeAvatar = (req, res) => {
   const { avatar } = req.body;
-  const { userId } = req.user._id;
+  const userId = req.user._id;
   User.findByIdAndUpdate(userId, { avatar }, { new: true })
     .then((user) => res.status(201).send(user))
     .catch((err) => res.status(500).send({ message: err.message }));
