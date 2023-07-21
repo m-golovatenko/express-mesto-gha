9fe93d810f2c8949af65304767c24244f915f484
Maria Golovatenko <pikch4y@yandex.com>
2023-07-21 12:37:13 +0300

    feat: add default state for name, about and avatar

--- models/user.js
+++ models/user.js
@@ -6,17 +6,17 @@ const userSchema = new mongoose.Schema({
     type: String,
     minlength: [2, 'Минимальная длина поля "name" - 2'],
     maxlength: [30, 'Максимальная длина поля "name" - 30'],
-    required: [true, 'Поле "name" должно быть заполнено'],
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
     minlength: [2, 'Минимальная длина поля "about" - 2'],
     maxlength: [30, 'Максимальная длина поля "about" - 30'],
-    required: [true, 'Поле "about" должно быть заполнено'],
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: [true, 'Поле "avatar" должно быть заполнено'],
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
       validator: (v) => validator.isURL(v),
       message: 'Некорректный URL',
