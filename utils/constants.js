bc621e3870b0ed246a6c1bd4b4030fa95363bfbf
Maria Golovatenko <pikch4y@yandex.com>
2023-07-28 17:58:51 +0300

    hotfix: delete global mode in regexp

--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,6 @@
 const SUCCESS_CODE = 200;
 const SUCCESS_CREATE_CODE = 201;
 
-const regExp = /^(https?:\/\/)?(www\.)?([\da-z]+)?\.([a-z.])([/\w .-_~:/?#[\]@!$&'()*+,;=]*)*\/?$/gmi;
+const regExp = /^(https?:\/\/)?(www\.)?([\da-z]+)?\.([a-z.])([/\w .-_~:/?#[\]@!$&'()*+,;=]*)*\/?$/i;
 
 module.exports = { SUCCESS_CODE, SUCCESS_CREATE_CODE, regExp };
